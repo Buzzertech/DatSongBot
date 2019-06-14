ff63798ec0c518bc0c1610f89f1aa437339c8985
Prasad Nayak <prasadnayak1006@gmail.com>
2019-06-15 01:33:37 +0530

    feat(config): add config for fetching audio

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+const config = {
+  SOUNDCLOUD_CLIENT_ID: process.env.SOUNDCLOUD_CLIENT_ID,
+  SOUNDCLOUD_SEARCH_TAGS: ['Chill', 'Trap'],
+  SOUNDCLOUD_SEARCH_LICENSE: 'cc-by',
+};
+
+export default config;
