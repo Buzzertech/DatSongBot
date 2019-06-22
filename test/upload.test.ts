8d77fb878c083e194c8c8d6b7683f5f6263f9b39
Buzzertech <prasadnayak1006@gmail.com>
2019-06-22 19:48:15 +0530

    test(upload): create a mock video file

--- test/upload.test.ts
+++ test/upload.test.ts
@@ -3,11 +3,13 @@ import { youtube_v3 } from 'googleapis';
 import { GaxiosResponse } from 'gaxios';
 import { tracks, imageData } from './__fixtures__/index.fixture';
 import { Track } from 'audio';
+import { createFileSync } from 'fs-extra';
 
 type VideoResponse = Partial<GaxiosResponse<youtube_v3.Schema$Video>>;
 
 describe('upload', () => {
   describe('#uploadVideo', () => {
+    beforeAll(() => createFileSync('/tmp/out.mp4'));
     it('will read and upload the video to youtube', async () => {
       const insertMock = jest.fn<Promise<VideoResponse>, any>().mockReturnValue(
         Promise.resolve({
